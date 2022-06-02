63c23b1c47cf5f406aeb62b71e49df207dba0963
liushooter <shooter.hero@gmail.com>
2022-06-02 13:05:03 +0800

    padded date

--- src/utils.ts
+++ src/utils.ts
@@ -10,10 +10,10 @@ export function toDate(_timestamp: string) {
   const formated_timestamp = _timestamp.replaceAll(',', '');
   const date: Date = new Date(Number(formated_timestamp));
 
-  const day = date.getDate();
-  const month = date.getMonth() + 1;
-  const hour = date.getHours();
-  const minute = date.getMinutes();
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const hour = String(date.getHours()).padStart(2, '0');
+  const minute = String(date.getMinutes()).padStart(2, '0');
 
   return `${month}-${day} ${hour}:${minute}`;
 }
